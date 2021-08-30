a820dc3c5e139951ee22bc73bd19a78f07e9e63c
yudan-yang1 <yudan.yang1@thoughtworks.com>
2021-08-30 14:19:24 +0800

    [yangyudan] #398 add uniqueid is null verify

--- modules/rest-api/src/main/java/org/openlmis/restapi/service/RestLoginService.java
+++ modules/rest-api/src/main/java/org/openlmis/restapi/service/RestLoginService.java
@@ -26,83 +26,88 @@ import java.util.List;
 @Service
 public class RestLoginService {
 
-    MessageService messageService = MessageService.getRequestInstance();
-    @Autowired
-    private UserAuthenticationService userAuthenticationService;
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private FacilityService facilityService;
-    @Autowired
-    private ProgramSupportedService programSupportedService;
-    @Autowired
-    private RestAppInfoService restAppInfoService;
+  MessageService messageService = MessageService.getRequestInstance();
+  @Autowired
+  private UserAuthenticationService userAuthenticationService;
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private FacilityService facilityService;
+  @Autowired
+  private ProgramSupportedService programSupportedService;
+  @Autowired
+  private RestAppInfoService restAppInfoService;
 
-    public LoginInformation login(String username, String password) {
-        authenticateUser(username, password);
-        LoginInformation loginInformation = getLoginInformation(username);
-        String uniqueId = LmisThreadLocalUtils.getHeader(LmisThreadLocalUtils.HEADER_UNIQUE_ID);
-        AppInfo appInfo = restAppInfoService.searchAppInfoByFacilityId(loginInformation.getFacilityId());
-        if(appInfo != null && !StringUtils.equals(uniqueId, appInfo.getUniqueId())){
-            throw new DataException("One facility can only sign in on one device. Your facility has signed in on another device.");
-        }
-        return loginInformation;
+  public LoginInformation login(String username, String password) {
+    authenticateUser(username, password);
+    LoginInformation loginInformation = getLoginInformation(username);
+    String uniqueId = LmisThreadLocalUtils.getHeader(LmisThreadLocalUtils.HEADER_UNIQUE_ID);
+    AppInfo appInfo = restAppInfoService.searchAppInfoByFacilityId(loginInformation.getFacilityId());
+    if (appInfo != null && appInfo.getUniqueId() != null && uniqueId != null &&
+        !StringUtils.equals(uniqueId, appInfo.getUniqueId())) {
+      throw new DataException(
+          "One facility can only sign in on one device. Your facility has signed in on another device.");
     }
+    return loginInformation;
+  }
 
-    private UserToken authenticateUser(String username, String password) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-        String userName = (String) authenticationToken.getPrincipal();
-        String pass = (String) authenticationToken.getCredentials();
+  private UserToken authenticateUser(String username, String password) {
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+        password);
+    String userName = (String) authenticationToken.getPrincipal();
+    String pass = (String) authenticationToken.getCredentials();
 
-        User user = new User();
-        user.setUserName(userName);
-        user.setPassword(pass);
+    User user = new User();
+    user.setUserName(userName);
+    user.setPassword(pass);
 
-        UserToken userToken = userAuthenticationService.authenticateUser(user);
+    UserToken userToken = userAuthenticationService.authenticateUser(user);
 
-        if (userToken.isAuthenticated()) {
-            return userToken;
-        } else {
-            throw new BadCredentialsException(messageService.message("error.authentication.failed"));
-        }
+    if (userToken.isAuthenticated()) {
+      return userToken;
+    } else {
+      throw new BadCredentialsException(messageService.message("error.authentication.failed"));
     }
+  }
 
-    private LoginInformation getLoginInformation(String username) {
-        User user = userService.getByUserName(username);
-        if (!user.isMobileUser()) {
-            throw new DataException("error.invalid.mobile.user");
-        }
-        Long facilityId = user.getFacilityId();
-        List<String> programs = FluentIterable.from(getProgramsSupportedByFacilityId(facilityId)).transform(new Function<ProgramSupported, String>() {
-            @Override
-            public String apply(ProgramSupported input) {
-                return input.getProgram().getCode();
-            }
+  private LoginInformation getLoginInformation(String username) {
+    User user = userService.getByUserName(username);
+    if (!user.isMobileUser()) {
+      throw new DataException("error.invalid.mobile.user");
+    }
+    Long facilityId = user.getFacilityId();
+    List<String> programs = FluentIterable.from(getProgramsSupportedByFacilityId(facilityId))
+        .transform(new Function<ProgramSupported, String>() {
+          @Override
+          public String apply(ProgramSupported input) {
+            return input.getProgram().getCode();
+          }
         }).toList();
-        return LoginInformation.prepareForREST(user, facilityService.getById(facilityId), programs);
-    }
+    return LoginInformation.prepareForREST(user, facilityService.getById(facilityId), programs);
+  }
 
-    private List<ProgramSupported> getProgramsSupportedByFacilityId(Long facilityId) {
-        if (facilityId != null) {
-            return programSupportedService.getActiveByFacilityId(facilityId);
-        } else {
-            throw new DataException("error.facility.unknown");
-        }
+  private List<ProgramSupported> getProgramsSupportedByFacilityId(Long facilityId) {
+    if (facilityId != null) {
+      return programSupportedService.getActiveByFacilityId(facilityId);
+    } else {
+      throw new DataException("error.facility.unknown");
     }
+  }
 
-    public List<FacilitySupportedProgram> getFacilitySupportedPrograms(Long facilityId) {
-        return FluentIterable.from(getProgramsSupportedByFacilityId(facilityId)).transform(new Function<ProgramSupported, FacilitySupportedProgram>() {
-            @Override
-            public FacilitySupportedProgram apply(ProgramSupported input) {
-                FacilitySupportedProgram program = new FacilitySupportedProgram();
-                program.setProgramCode(input.getProgram().getCode());
-                program.setProgramName(input.getProgram().getName());
-                program.setIsSupportEmergency(input.getProgram().getIsSupportEmergency());
-                if (input.getProgram().getParent() != null) {
-                    program.setParentCode(input.getProgram().getParent().getCode());
-                }
-                return program;
+  public List<FacilitySupportedProgram> getFacilitySupportedPrograms(Long facilityId) {
+    return FluentIterable.from(getProgramsSupportedByFacilityId(facilityId))
+        .transform(new Function<ProgramSupported, FacilitySupportedProgram>() {
+          @Override
+          public FacilitySupportedProgram apply(ProgramSupported input) {
+            FacilitySupportedProgram program = new FacilitySupportedProgram();
+            program.setProgramCode(input.getProgram().getCode());
+            program.setProgramName(input.getProgram().getName());
+            program.setIsSupportEmergency(input.getProgram().getIsSupportEmergency());
+            if (input.getProgram().getParent() != null) {
+              program.setParentCode(input.getProgram().getParent().getCode());
             }
+            return program;
+          }
         }).toList();
-    }
+  }
 }
