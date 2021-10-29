2862a12d8634a1f38d48f20bf5a0e4452cada554
yudan-yang1 <yudan.yang1@thoughtworks.com>
2021-10-29 12:43:31 +0800

    [yangyudan] #396 filter province all for expiry date page

--- modules/migration/src/main/resources/db/migration/new_scripts/V804__drop_split_lot_expiry_dates_2021_and_some_function.sql
+++
@@ -1,7 +0,0 @@
-DROP MATERIALIZED VIEW IF EXISTS public.vw_lot_expiry_dates_before_20211001;
-DROP MATERIALIZED VIEW IF EXISTS public.vw_lot_expiry_dates_after_20211001;
-DROP FUNCTION refresh_vw_lot_expiry_dates_before_20211001();
-DROP FUNCTION refresh_vw_lot_expiry_dates_after_20211001();
-DROP FUNCTION refresh_period_movements_before_20210721();
-DROP FUNCTION refresh_period_soh_and_cmm_after_20210721();
-DROP FUNCTION refresh_period_movements_after_20210721();
