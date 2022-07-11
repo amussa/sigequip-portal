a334f7eeb58300b37d79303d0db140a0a9a5f2c7
yudan-yang1 <yudan.yang1@thoughtworks.com>
2022-07-11 16:54:41 +0800

    [yangyudan] #593 split period table from 20220621

--- modules/openlmis-web/src/main/webapp/public/js/reports/shared/controller/base-product-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/shared/controller/base-product-report-controller.js
@@ -360,10 +360,10 @@ function BaseProductReportController($scope, $filter, ProductReportService, $cac
   };
 
   $scope.pickMVs = function(period) {
-    if (period.periodStart.getTime()  >= new Date("2022/01/21").getTime()){
-      return 'vw_period_movements_after_20220121';
+    if (period.periodStart.getTime()  >= new Date("2022/06/21").getTime()){
+      return 'vw_period_movements_after_20220621';
     }else {
-      return 'vw_period_movements_before_20220121';
+      return 'vw_period_movements_before_20220621';
     }
   };
 
